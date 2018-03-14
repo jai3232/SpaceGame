0e4fa8cc4d8a9f40d96f4ff3c60b8936215435b0
jai <jai3232@gmail.com>
2018-03-14 16:39:55 +0800

    Key Control

--- scripts/main.js
+++ scripts/main.js
@@ -1,154 +1,170 @@
 
-$(document).ready(function(){
-	var canvas;
-	var canvascontext;
-	var ballX = 50;
-	var ballY = 50;
-	var ballXSpeed = 5;
-	var ballYSpeed = 5;
-	var fps = 30;
-	var paddle1Y = 250;
-	var paddleHeight = 100;
 
-	//window.onload = function(){
-		canvas = document.getElementById("game");
-		ctx = canvas.getContext("2d");
+var canvas;
+var ctx;
+var ballX = 50;
+var ballY = 50;
+var ballXSpeed = 5;
+var ballYSpeed = 5;
+var fps = 30;
+var paddle1Y = 250;
+var paddleHeight = 100;
+var spaceship = new Image();
+var spaceshipX;
+var spaceshipY;
+
+window.onload = function(){
+	canvas = document.getElementById("game");
+	ctx = canvas.getContext("2d");
+	
+	var background = new Image();
+	var background2 = new Image();
+	var logo = new Image();
+	var plus = new Image();
+	var minus = new Image();
+	var sound = new Image();
+	var sound_down = new Image();
+	var play = new Image();
+	var pause = new Image();
+	var timer = new Image();
+	var fuel = new Image();
+	var fuel_icon = new Image();
+	var mars = new Image();
+	var jupiter = new Image();
+	var saturn = new Image();
+
+	background.src = "images/background.jpg";
+	background2.src = "images/background.jpg";
+	logo.src = "images/logo.png";
+	plus.src = "images/plus.png";
+	minus.src = "images/minus.png";
+	sound.src = "images/sound.png",
+	sound_down.src = "images/sound-down.png";
+	play.src = "images/play.png";
+	pause.src = "images/pause.png";
+	timer.src = "images/timer.png";
+	fuel.src = "images/fuel.png";
+	fuel_icon.src = "images/fuel-icon.png";
+	mars.src = "images/mars.png";
+	jupiter.src = "images/jupiter.png";
+	saturn.src = "images/saturn.png";
+	spaceship.src = "images/spaceship.png";
+
+	spaceshipX = canvas.width / 100;
+	spaceshipY = canvas.height / 2;
+
+
+
+	background.onload = function(){
+		ctx.drawImage(background, 0, 0);
+		ctx.drawImage(background2, 0 - canvas.width, 0);
+		ctx.fillStyle = "#096643"; //#090043
+		ctx.fillRect(0, 0,  canvas.width, 40);
+		ctx.fillStyle = "white"; 
+		ctx.font = "25px Arial";
+		ctx.fillText("Font",140,28);
 		
-		var background = new Image();
-		var background2 = new Image();
-		var logo = new Image();
-		var plus = new Image();
-		var minus = new Image();
-		var sound = new Image();
-		var sound_down = new Image();
-		var play = new Image();
-		var pause = new Image();
-		var timer = new Image();
-		var fuel = new Image();
-		var fuel_icon = new Image();
-		var mars = new Image();
-		var jupiter = new Image();
-		var saturn = new Image();
+	}
+	logo.onload = function() {
+		ctx.drawImage(logo, 0, 0, 120, 80);
+	}
+	plus.onload = function() {
+		ctx.drawImage(plus, 200, 5, 30, 30);
+	}
+	minus.onload = function() {
+		ctx.drawImage(minus, 240, 5, 30, 30);
+	}
+	sound.onload = function() {
+		ctx.drawImage(sound, 310, 5, 25, 25);
+	}
+	sound_down.onload = function() {
+		ctx.drawImage(sound_down, 340, 5, 28, 28);
+	}
+	play.onload = function() {
+		ctx.drawImage(play, 390, 5, 30, 30);
+	}
+	pause.onload = function() {
+		ctx.drawImage(pause, 420, 5, 30, 30);
+	}
+	timer.onload = function() {
+		ctx.drawImage(timer, 470, 5, 30, 30);
+	}
+	fuel.onload = function() {
+		ctx.drawImage(fuel, 530, 5, 30, 30);	
+	}
+	fuel_icon.onload = function() {
+		ctx.drawImage(fuel_icon, 700, 200, 50, 50);
+	}
+	// mars.onload = function() {
+	// 	ctx.drawImage(mars, 750, 50, 150, 150);
+	// }
+	// jupiter.onload = function() {
+	// 	//ctx.drawImage(jupiter,530, 150, 100, 100);	
+	// }
 
-		background.src = "images/background.jpg";
-		background2.src = "images/background.jpg";
-		logo.src = "images/logo.png";
-		plus.src = "images/plus.png";
-		minus.src = "images/minus.png";
-		sound.src = "images/sound.png",
-		sound_down.src = "images/sound-down.png";
-		play.src = "images/play.png";
-		pause.src = "images/pause.png";
-		timer.src = "images/timer.png";
-		fuel.src = "images/fuel.png";
-		fuel_icon.src = "images/fuel-icon.png";
-		mars.src = "images/mars.png";
-		jupiter.src = "images/jupiter.png";
-		saturn.src = "images/saturn.png";
+	var bspeed = 0;
+	var jupiterSpeed = 500;
+	var marsSpeed = 750;
+	var saturnSpeed = 880;
 
+	function draw() {
 
-		background.onload = function(){
-			ctx.drawImage(background, 0, 0);
-			ctx.drawImage(background2, 0 - canvas.width, 0);
-			ctx.fillStyle = "#096643"; //#090043
-			ctx.fillRect(0, 0,  canvas.width, 40);
-			ctx.fillStyle = "white"; 
-			ctx.font = "25px Arial";
-			ctx.fillText("Font",140,28);
-			
-		}
-		logo.onload = function() {
-			ctx.drawImage(logo, 0, 0, 120, 80);
-		}
-		plus.onload = function() {
-			ctx.drawImage(plus, 200, 5, 30, 30);
-		}
-		minus.onload = function() {
-			ctx.drawImage(minus, 240, 5, 30, 30);
-		}
-		sound.onload = function() {
-			ctx.drawImage(sound, 310, 5, 25, 25);
-		}
-		sound_down.onload = function() {
-			ctx.drawImage(sound_down, 340, 5, 28, 28);
-		}
-		play.onload = function() {
-			ctx.drawImage(play, 390, 5, 30, 30);
-		}
-		pause.onload = function() {
-			ctx.drawImage(pause, 420, 5, 30, 30);
-		}
-		timer.onload = function() {
-			ctx.drawImage(timer, 470, 5, 30, 30);
-		}
-		fuel.onload = function() {
-			ctx.drawImage(fuel, 530, 5, 30, 30);	
-		}
-		fuel_icon.onload = function() {
-			ctx.drawImage(fuel_icon, 700, 200, 50, 50);
-		}
-		// mars.onload = function() {
-		// 	ctx.drawImage(mars, 750, 50, 150, 150);
-		// }
-		// jupiter.onload = function() {
-		// 	//ctx.drawImage(jupiter,530, 150, 100, 100);	
-		// }
+		//clearing the canvas
+		ctx.clearRect(0, 40, canvas.width, canvas.height);
+		ctx.drawImage(background, bspeed, 40);
+		ctx.drawImage(background2, bspeed + canvas.width, 40);
+		ctx.drawImage(logo, 0, 0, 120, 80);
+		ctx.drawImage(jupiter, jupiterSpeed, 250, 100, 100);	
+		ctx.drawImage(mars, marsSpeed, 50, 150, 150);
+		ctx.drawImage(saturn, saturnSpeed, 500, 100, 60);
+		//ctx.drawImage(mars, 800, 200, 50, 50);
 
-		var bspeed = 0;
-		var jupiterSpeed = 500;
-		var marsSpeed = 750;
-		var saturnSpeed = 880;
+		// drawing a black circle
+		// ctx.beginPath();
+		// ctx.arc(bspeed + canvas.width, 100, 25, 0, 2*Math.PI);
+		// ctx.fillStyle = "white";
+		// ctx.fill();
 
-		function draw() {
+		// incrementing the x coordinate by one on each frame
+		bspeed -= 0.5;
+		jupiterSpeed -= 1;
+		marsSpeed -= 0.25;
+		saturnSpeed -= 3;
 
-			//clearing the canvas
-			ctx.clearRect(0, 40, canvas.width, canvas.height);
-			ctx.drawImage(background, bspeed, 40);
-			ctx.drawImage(background2, bspeed + canvas.width, 40);
-			ctx.drawImage(logo, 0, 0, 120, 80);
-			ctx.drawImage(jupiter, jupiterSpeed, 250, 100, 100);	
-			ctx.drawImage(mars, marsSpeed, 50, 150, 150);
-			ctx.drawImage(saturn, saturnSpeed, 500, 100, 60);
-			//ctx.drawImage(mars, 800, 200, 50, 50);
+		// if x coordinate exceeds the width, reset it to 0.
+		if(bspeed < -canvas.width) bspeed = 0;
+		if(jupiterSpeed < -100) jupiterSpeed = canvas.width;
+		if(marsSpeed < -150) marsSpeed = canvas.width;
+		if(saturnSpeed < -150) saturnSpeed = canvas.width;
 
-			// drawing a black circle
-			ctx.beginPath();
-			ctx.arc(bspeed + canvas.width, 100, 25, 0, 2*Math.PI);
-			ctx.fillStyle = "white";
-			ctx.fill();
+		// // call draw() again when a new frame needs to be drawn
+		drawSpaceShip();
+		requestAnimationFrame(draw);
+	}
 
-			// incrementing the x coordinate by one on each frame
-			bspeed -= 0.5;
-			jupiterSpeed -= 1;
-			marsSpeed -= 0.25;
-			saturnSpeed -= 2;
+	draw();
 
-			// if x coordinate exceeds the width, reset it to 0.
-			if(bspeed < -canvas.width) bspeed = 0;
-			if(jupiterSpeed < -100) jupiterSpeed = canvas.width;
-			if(marsSpeed < -150) marsSpeed = canvas.width;
-			if(saturnSpeed < -150) saturnSpeed = canvas.width;
+	//setInterval(update, 1000/fps);
+	canvas.addEventListener('mousemove', function(e){
+		// var mousePos = calculateMousePos(e);
+		// paddle1Y = mousePos.y - paddleHeight/2;
+	});
+	canvas.addEventListener('mousedown', clicked, false);
 
-			// // call draw() again when a new frame needs to be drawn
-			requestAnimationFrame(draw);
-		}
+	document.addEventListener('keydown', keyPressed);
 
-		draw();
+};
 
-		setInterval(update, 1000/fps);
-		canvas.addEventListener('mousemove', function(e){
-			// var mousePos = calculateMousePos(e);
-			// paddle1Y = mousePos.y - paddleHeight/2;
-		});
-		canvas.addEventListener('mousedown', clicked, false);
-
-	//};
-});
 
 function update() {
 	
 }
 
+function drawSpaceShip() {
+	// if(x === undefined) { x = 20;}
+	// if(y === undefined) { y = 300;}
+	ctx.drawImage(spaceship, spaceshipX, spaceshipY, 100, 50);
+}
 
 function clicked(e) {
 	var canvas = document.getElementById("game");
@@ -158,6 +174,32 @@ function clicked(e) {
 	if(x >= 200 && x <=230 && y >= 5 && y <= 30)
 		alert("+");
 	//console.log("X:"+x+" y:"+y);
+}
+
+function keyPressed(e) {
+    switch(e.keyCode)
+    {
+        case 37:
+            // Left Arrow key
+            spaceshipX -= 5;
+            console.log('Left:'+spaceshipX);
+            break;
+        case 39:
+            // Right Arrow key
+            spaceshipX += 5;
+            console.log('Right:'+spaceshipX);
+            break;
+        case 38:
+            // Up Arrow key
+            spaceshipY -= 5;
+            console.log('Up:'+spaceshipY);
+            break;
+         case 40:
+            // Down Arrow key
+            spaceshipY += 5;
+            console.log('Down:'+spaceshipY);
+            break;
+    }
 }
 
 function Button(canvas, image, x, y, w, h) {
